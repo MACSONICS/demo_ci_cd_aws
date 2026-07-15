b5b4cf64df95d0d5508dd83e7e5283ba4ad5923b
markson <maigbodi@gmail.com>
2026-07-15 20:57:18 +0100

    fixed key location

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "public_key_path" {
-  description = "Path to your public SSH key"
-  default     = "~/.ssh/awsacademy.pub"
+  default = "~/.ssh/awsacademy.pub"
 }
+
 
 variable "instance_type" {
   default = "t2.micro"
