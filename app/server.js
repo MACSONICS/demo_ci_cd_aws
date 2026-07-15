912798b23371bfa85be40c21f4f9e16b58763cca
markson <maigbodi@gmail.com>
2026-07-15 21:10:54 +0100

    new key

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from CI/CD pipeline!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline! works'));
 
 app.listen(3000);
