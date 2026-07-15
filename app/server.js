48351535e6c3f09284176d46ff397618377dbcdb
markson <maigbodi@gmail.com>
2026-07-15 21:19:42 +0100

    delete sg

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from CI/CD pipeline! works'));
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline! '));
 
 app.listen(3000);
